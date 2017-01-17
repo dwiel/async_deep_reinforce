1b78688980cedc827e89b1d4a570acb94b7022fc
Zach Dwiel <zdwiel@plotwatt.com>
2017-01-16 20:51:34 -0800

    update ACTION_SIZE for new game state

--- constants.py
+++ constants.py
@@ -9,8 +9,7 @@ INITIAL_ALPHA_LOW = 1e-4  # log_uniform low limit for learning rate
 INITIAL_ALPHA_HIGH = 1e-2  # log_uniform high limit for learning rate
 
 PARALLEL_SIZE = 8  # parallel thread size
-ROM = "pong.bin"  # action size = 3
-ACTION_SIZE = 3  # action size
+ACTION_SIZE = 4  # action size
 
 INITIAL_ALPHA_LOG_RATE = 0.4226  # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
 GAMMA = 0.99  # discount factor for rewards
@@ -18,4 +17,4 @@ ENTROPY_BETA = 0.01  # entropy regurarlization constant
 MAX_TIME_STEP = 10 * 10**7
 GRAD_NORM_CLIP = 40.0  # gradient norm clipping
 USE_GPU = True  # To use GPU, set True
-USE_LSTM = True  # True for A3C LSTM, False for A3C FF
+USE_LSTM = False  # True for A3C LSTM, False for A3C FF
