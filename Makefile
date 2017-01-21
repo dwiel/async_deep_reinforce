6d0dd223bbf564aa75856e7b036863a53093952d
Zach Dwiel <zdwiel@plotwatt.com>
2017-01-21 09:11:47 -0800

    add missing files

---
+++ Makefile
@@ -0,0 +1,11 @@
+build:
+	@docker build -t async_deep_reinforce .
+
+run: build
+	@docker run --rm -it async_deep_reinforce
+
+shell: build
+	@docker run --rm -it async_deep_reinforce /bin/bash
+
+test: build
+	@docker run --rm -t async_deep_reinforce_test .
---
+++ test_alt_game_state.py
@@ -0,0 +1,82 @@
+import pytest
+import numpy as np
+from alt_game_state import GameState, UnexpectedCopyWindowInputShape
+
+
+def test_tiny():
+    gs = GameState(view_width=1, view_height=1, board_width=1, board_height=1)
+
+    x_t = np.array([2]).reshape((1, 1))
+    gs._copy_window(x_t)
+
+    assert x_t[0, 0] == 1
+
+
+def test_incorrect_view_shape():
+    gs = GameState(view_width=1, view_height=1, board_width=3, board_height=3)
+
+    x_t = np.array([3, 3]).reshape((1, 2))
+    with pytest.raises(UnexpectedCopyWindowInputShape):
+        gs._copy_window(x_t)
+
+
+def test_tiny_view():
+    BOARD_WIDTH = 5
+    BOARD_HEIGHT = 5
+    gs = GameState(
+        view_width=1,
+        view_height=1,
+        board_width=BOARD_WIDTH,
+        board_height=BOARD_HEIGHT
+    )
+
+    assert gs.board_position_x == BOARD_WIDTH / 2
+    assert gs.board_position_y == BOARD_WIDTH / 2
+
+    x_t = np.array([3]).reshape((1, 1))
+
+    gs._copy_window(x_t)
+    # assert x_t[0, 0] == 1
+
+    image = np.zeros((BOARD_WIDTH, BOARD_HEIGHT))
+    for gs.board_position_x in range(BOARD_WIDTH):
+        for gs.board_position_y in range(BOARD_HEIGHT):
+            gs._copy_window(x_t)
+            image[gs.board_position_x, gs.board_position_y] = x_t[0, 0]
+
+    target = np.zeros((BOARD_WIDTH, BOARD_HEIGHT), dtype=np.float32)
+    target[BOARD_WIDTH / 2, BOARD_WIDTH / 2] = 1
+    np.testing.assert_allclose(image, target)
+
+
+def test_wider_view():
+    VIEW_WIDTH = 5
+    VIEW_HEIGHT = 5
+
+    gs = GameState(
+        view_width=VIEW_WIDTH,
+        view_height=VIEW_HEIGHT,
+        board_width=3,
+        board_height=3
+    )
+
+    assert gs.board_position_x == 1
+    assert gs.board_position_y == 1
+    np.testing.assert_allclose(gs.height, [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
+    ])
+
+    x_t = np.random.random((VIEW_HEIGHT, VIEW_WIDTH))
+
+    gs._copy_window(x_t)
+    np.testing.assert_allclose(
+        x_t, [
+            [-1, -1, -1, -1, -1],
+            [-1, 0, 0, 0, -1],
+            [-1, 0, 1, 0, -1],
+            [-1, 0, 0, 0, -1],
+            [-1, -1, -1, -1, -1],
+        ]
+    )
